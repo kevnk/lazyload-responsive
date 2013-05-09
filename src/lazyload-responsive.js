c18479274b6b4bc6f32b62b482faa6878d8ae8e7
Kevin Kirchner <kevin@classyllama.com>
2013-05-09 09:30:16 -0500

    (partly) laying foundation for jquery plugin and plugin tests

--- src/lazyload-responsive.js
+++ src/lazyload-responsive.js
@@ -6,33 +6,77 @@
  * Licensed under the MIT license.
  */
 
-(function($) {
+!(function($, window, undefined) {
+    
+    "use strict"
+    
+    /* LAZYLOADRESPONSIVE PUBLIC CLASS DEFINITION
+     * ========================================== */
+    
+    var LazyloadResponsive = function( element, options ) {
+        this.init(element, options);
+    };
+    
+    LazyloadResponsive.prototype = {
+        constructor: LazyloadResponsive,
+        init: function( element, options ) {
+            this.$element = $(element);
+            this.options = this.getOptions(options);
+        },
+        getOptions: function( options ) {
+            return $.extend({}, $.fn.lazyloadResponsive.defaults, options, this.$element.data());
+        }
+    };
+    
+    /* LAZYLOADRESPONSIVE PLUGIN DEFINITION 
+     * ==================================== */
+    
+    // Collection method
+    $.fn.lazyloadResponsive = function( option ) {
+        return this.each(function () {
+            var $this = $(this)
+              , data = $this.data('lzldrspv')
+              , options = typeof option == 'object' && option
+            if (!data) $this.data('lzldrspv', (data = new LazyloadResponsive(this, options)));
+            if (typeof option == 'string') data[option]();
+        });
+    };
 
-  // Collection method.
-  $.fn.awesome = function() {
-    return this.each(function(i) {
-      // Do something awesome to each selected element.
-      $(this).html('awesome' + i);
+    $.fn.lazyloadResponsive.Constructor = LazyloadResponsive;
+
+    // Static method default options.
+    $.fn.lazyloadResponsive.defaults = {
+        srcAttr: 'data-lzld-src',
+        offset: 200, // distance (px) below the fold where images will be loaded
+        highResThreshold: 1.5, // any pixel ratio >= this number will be flagged as high-res
+        throttleInterval: 20, // throttled interval for scroll and resize events
+        useGlobalImgConfig: false, // if `false`, the script will look for the following imgConfig on each lzld img (e.g. img.getAttribute('data-lzld-highressuffix') || imgConfig.highressuffix - which takes the script longer to process); setting to `true` is the fastest option
+        findSmallerImgs: true, // forces script to look for and load smaller images as viewport is resized to a smaller size
+        imgConfig: {
+            highressuffix: '@2x', // e.g. imagename@2x.jpg or imagename_400@2x.jpg would be the high-res images
+            loadevent: ['scroll','resize'], // you may want to load the images on a custom event; this is where you do that
+            longfallback: true, // will look for all smaller images before loading the original (and largest image)
+            lowres: false, // forces script to **not** look for high-res images
+            sizedown: false, // by default images are sized up; this option forces it to get an image larger than the viewport and shrink it; NOTE: setting to `true` will load larger images and increase pageload
+            widthfactor: 200 // looks for images with the following naming convention [real-image-src]_[factor of widthfactor].[file-extenstion]
+        }
+    };
+    
+    /* LAZYLOADRESPONSIVE DATA API
+     * =========================== */
+    
+    $(function () {
+        // Onload
+        // Scroll
+        // Resize
+    /*
+        $('body').on(function (e) {
+            var $this = $(this);
+            if ($this.data('lzldrspv')) return;
+            e.preventDefault();
+            $this.lazyloadResponsive($this.data());
+        })
+    */
     });
-  };
 
-  // Static method.
-  $.awesome = function(options) {
-    // Override default options with passed-in options.
-    options = $.extend({}, $.awesome.options, options);
-    // Return something awesome.
-    return 'awesome' + options.punctuation;
-  };
-
-  // Static method default options.
-  $.awesome.options = {
-    punctuation: '.'
-  };
-
-  // Custom selector.
-  $.expr[':'].awesome = function(elem) {
-    // Is this element awesome?
-    return $(elem).text().indexOf('awesome') !== -1;
-  };
-
-}(jQuery));
+}(jQuery, this));
--- test/lazyload-responsive_test.js
+++ test/lazyload-responsive_test.js
@@ -20,6 +20,86 @@
       throws(block, [expected], [message])
   */
 
+    // Lazyload-Responsive
+    module('LazyloadResponsive', {
+        setup: function() {
+            this.elems = $('img[data-lzld-src]');
+        }
+    });
+    
+    test('is chainable', function() {
+      expect(1);
+      // Not a bad test to run on collection methods.
+      strictEqual(this.elems.lazyloadResponsive(), this.elems, 'should be chainable');
+    });
+    
+    module('Request Management', {
+        setup: function() {
+            var that = this;
+            that.requestedSrcs = [];
+            // @see http://stackoverflow.com/questions/3596583/javascript-detect-an-ajax-event
+            that.ajaxListener = new Object();
+            that.ajaxListener.tempOpen = XMLHttpRequest.prototype.open;
+            that.ajaxListener.tempSend = XMLHttpRequest.prototype.send;
+            that.ajaxListener.callback = function () {
+                // TODO: if method was one to look for an image, then run this
+                if (true) {
+                    that.requestedSrcs.push(this.url);
+                }
+              // this.method :the ajax method used
+              // this.url    :the url of the requested script (including query string, if any) (urlencoded) 
+              // this.data   :the data sent, if any ex: foo=bar&a=b (urlencoded)
+            }
+
+            XMLHttpRequest.prototype.open = function(a,b) {
+              if (!a) var a='';
+              if (!b) var b='';
+              that.ajaxListener.tempOpen.apply(this, arguments);
+              that.ajaxListener.method = a;  
+              that.ajaxListener.url = b;
+              if (a.toLowerCase() == 'get') {
+                that.ajaxListener.data = b.split('?');
+                that.ajaxListener.data = that.ajaxListener.data[1];
+              }
+            }
+
+            XMLHttpRequest.prototype.send = function(a,b) {
+              if (!a) var a='';
+              if (!b) var b='';
+              that.ajaxListener.tempSend.apply(this, arguments);
+              if(that.ajaxListener.method.toLowerCase() == 'post')that.ajaxListener.data = a;
+              that.ajaxListener.callback();
+            }
+        }
+    });
+
+    // No file should ever be requested more than once
+    asyncTest('has no duplicate image requests', 1, function(){
+        // TODO: make this an on method that makes sense
+        that.ajaxListener.on('readystatechange', function(data){
+            QUnit.log(data)
+            start();
+        })
+    })
+
+// Event: Onload
+// Images within range should be loaded AND resized
+
+// Event: Scroll
+// Images within range should be loaded AND resized
+// Call on scroll should be throttled
+// Option related tests
+
+// Event: Resize
+// Images within range should be loaded AND resized
+// Call on resize should be throttled
+// Option related tests
+
+// Dynamically Added Content
+// Images within range should be loaded AND resized
+
+
+/*
   module('jQuery#awesome', {
     // This will run before each test in this module.
     setup: function() {
@@ -30,20 +110,20 @@
   test('is chainable', function() {
     expect(1);
     // Not a bad test to run on collection methods.
-    strictEqual(this.elems.awesome(), this.elems, 'should be chainable');
+    strictEqual(this.elems.lazyloadResponsive(), this.elems, 'should be chainable');
   });
 
   test('is awesome', function() {
     expect(1);
-    strictEqual(this.elems.awesome().text(), 'awesome0awesome1awesome2', 'should be awesome');
+    strictEqual(this.elems.lazyloadResponsive().text(), 'awesome0awesome1awesome2', 'should be awesome');
   });
 
-  module('jQuery.awesome');
+  module('jQuery.lazyloadResponsive');
 
   test('is awesome', function() {
     expect(2);
-    strictEqual($.awesome(), 'awesome.', 'should be awesome');
-    strictEqual($.awesome({punctuation: '!'}), 'awesome!', 'should be thoroughly awesome');
+    strictEqual($.lazyloadResponsive(), 'awesome.', 'should be awesome');
+    strictEqual($.lazyloadResponsive({punctuation: '!'}), 'awesome!', 'should be thoroughly awesome');
   });
 
   module(':awesome selector', {
@@ -58,5 +138,6 @@
     // Use deepEqual & .get() when comparing jQuery objects.
     deepEqual(this.elems.filter(':awesome').get(), this.elems.last().get(), 'knows awesome when it sees it');
   });
+*/
 
 }(jQuery));
